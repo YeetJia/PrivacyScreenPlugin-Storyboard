4279e1c2a953f3ddc26ce268ddef60118789ef42
Liew Ken Leong <>
2020-07-03 13:01:18 +0800

    set flag for android

--- src/android/PrivacyScreenPlugin.java
+++ src/android/PrivacyScreenPlugin.java
@@ -7,20 +7,11 @@
 package org.devgeeks.privacyscreen;
 
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaWebView;
 
 import android.app.Activity;
-import android.view.Window;
 import android.view.WindowManager;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import android.content.Context;
-import android.os.Bundle;
 
 /**
  * This class sets the FLAG_SECURE flag on the window to make the app
@@ -32,6 +23,14 @@ public class PrivacyScreenPlugin extends CordovaPlugin {
   public void initialize(CordovaInterface cordova, CordovaWebView webView) {
     super.initialize(cordova, webView);
     Activity activity = this.cordova.getActivity();
-    activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+    if (isPrivacyScreenEnabled()) {
+      activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+    }
+  }
+  /**
+   * Shorter way to check value of "PrivacyScreenEnabledAndroidFlag" preference.
+   */
+  private boolean isPrivacyScreenEnabled () {
+    return preferences.getBoolean("PrivacyScreenEnabledAndroidFlag", false);
   }
 }
